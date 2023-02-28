2c4358126d19fcfea8c63ab96fc84627664d1867
NoNameLmao <egor5553049@gmail.com>
2023-02-28 21:14:55 +0300

    "beautify" cli

--- todo.py
+++ todo.py
@@ -1,4 +1,8 @@
 import os
+from colorama import init, Fore, Style
+
+# Initialize colorama for Windows support
+init()
 
 # Define the filename to store tasks
 filename = "tasks.txt"
@@ -17,36 +21,36 @@ def save_tasks():
         f.write('\n'.join(tasks))
 
 def add_task():
-    task = input("Enter task: ")
+    task = input(Fore.GREEN + "Enter task: " + Style.RESET_ALL)
     tasks.append(task)
     save_tasks()
-    print("Task added successfully!\n")
+    print(Fore.YELLOW + "Task added successfully!\n" + Style.RESET_ALL)
 
 def view_tasks():
     if len(tasks) == 0:
-        print("No tasks found!\n")
+        print(Fore.YELLOW + "No tasks found!\n" + Style.RESET_ALL)
     else:
-        print("Tasks:")
+        print(Fore.GREEN + "Tasks:\n" + Style.RESET_ALL)
         for i, task in enumerate(tasks):
-            print(f"{i+1}. {task}")
+            print(Fore.YELLOW + f"{i+1}. {task}" + Style.RESET_ALL)
         print()
 
 def complete_task():
     view_tasks()
-    index = int(input("Enter task number to complete: "))
+    index = int(input(Fore.GREEN + "Enter task number to complete: " + Style.RESET_ALL))
     if index < 1 or index > len(tasks):
-        print("Invalid task number!\n")
+        print(Fore.RED + "Invalid task number!\n" + Style.RESET_ALL)
     else:
         task = tasks.pop(index-1)
         save_tasks()
-        print(f"{task} completed successfully!\n")
+        print(Fore.YELLOW + f"{task} completed successfully!\n" + Style.RESET_ALL)
 
 while True:
-    print("1. Add task")
+    print(Fore.GREEN + "1. Add task")
     print("2. View tasks")
     print("3. Complete task")
-    print("4. Quit")
-    choice = int(input("Enter choice: "))
+    print("4. Quit" + Style.RESET_ALL)
+    choice = int(input(Fore.GREEN + "Enter choice: " + Style.RESET_ALL))
     
     if choice == 1:
         add_task()
@@ -55,7 +59,7 @@ while True:
     elif choice == 3:
         complete_task()
     elif choice == 4:
-        print("Goodbye!")
+        print(Fore.GREEN + "Goodbye!\n" + Style.RESET_ALL)
         break
     else:
-        print("Invalid choice!\n")
+        print(Fore.RED + "Invalid choice!\n" + Style.RESET_ALL)
